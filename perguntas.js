e79eb93fbf913ec8ee896d1ffee4716065346fce
Ericafh <h.erica@escola.pr.gov.br>
2025-08-14 08:28:08 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,25 +1,22 @@
 criaCartao(
     'Medicina Veterinária',
-    'O que faz um Medico Veterinário?',
-    'cuida da saúde e bem-estar dos animais, diagnosticando e tratando doenças, realizando cirurgias, prevenindo enfermidades, promovendo a saúde pública e atuando em diversas áreas como clínica, cirurgia, produção animal, inspeção sanitária e pesquisa. 
-'
+    'O que faz um Médico Veterinário?',
+    'Cuida da saúde e bem-estar dos animais, diagnosticando e tratando doenças, realizando cirurgias, prevenindo enfermidades, promovendo a saúde pública e atuando em diversas áreas como clínica, cirurgia, produção animal, inspeção sanitária e pesquisa.' 
 )
 
 criaCartao(
     'Medicina Veterinária',
     'Como examinar um animal doente?',
-    'através de uma avaliação clínica detalhada, que inclui anamnese (entrevista com o tutor), exame físico e, se necessário, exames complementares como exames laboratoriais ou de imagem. O objetivo é identificar a causa da doença e determinar o tratamento adequado. 
-'
+    'Através de uma avaliação clínica detalhada, que inclui anamnese (entrevista com o tutor), exame físico e, se necessário, exames complementares como exames laboratoriais ou de imagem. O objetivo é identificar a causa da doença e determinar o tratamento adequado.' 
 )
 
 criaCartao(
     'Medicina Veterinária',
     'Qual é a função da Medicina Veterinária?',
-    'promover a saúde e o bem-estar animal, tanto em animais domésticos quanto selvagens, além de contribuir para a saúde pública e a sustentabilidade ambiental.'
+    'Promover a saúde e o bem-estar animal, tanto em animais domésticos quanto selvagens, além de contribuir para a saúde pública e a sustentabilidade ambiental.'
 )
     criaCartao(
     'Medicina Veterinária',
-    'Como aprender novas praticas ou se aperfeiçoar?',
-    'participar de cursos, workshops e conferências, buscar especializações e ter uma rede de contatos profissionais. Além disso, a leitura de artigos científicos, a participação em grupos de discussão e a utilização de plataformas online de aprendizado são estratégias eficazes. 
-'
+    'Como aprender novas práticas ou se aperfeiçoar?',
+    'Participar de cursos, workshops e conferências, buscar especializações e ter uma rede de contatos profissionais. Além disso, a leitura de artigos científicos, a participação em grupos de discussão e a utilização de plataformas online de aprendizado são estratégias eficazes.' 
 )
